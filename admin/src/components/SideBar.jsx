4738e8d23acf3a09824b87eedb850fa8bfbfbd5e
ishukhan <99346927+ishukhan@users.noreply.github.com.>
2025-05-23 11:30:07 +0530

    assets

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import addProducticon from '../assets/addProduct.png'
+import addProductIcon from '../assets/addProducts.png'
 import allProduct from "../assets/productlist.png"
 
 const SideBar = () => {
@@ -7,7 +7,7 @@ const SideBar = () => {
     <div className='py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6'>
      <Link to="/addProduct">
       <button className='flexCenter gap-2 rounded-md bg-primary h-12 w-40 xs:w-44 medium-16'>
-        <img src={addProducticon} alt="" height={50} width={50}/>
+        <img src={addProductIcon} alt="" height={50} width={50}/>
         <span>Add product</span>
       </button>
      </Link>
